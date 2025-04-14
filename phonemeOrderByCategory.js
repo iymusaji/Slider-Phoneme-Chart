369d928481293063a88befe6b764855bd94e0b01
iymusaji <36754295+iymusaji@users.noreply.github.com>
2025-04-14 09:55:25 -0500

    Add files via upload

---
+++ phonemeOrderByCategory.js
@@ -0,0 +1,8 @@
+const phonemeOrderByCategory = {
+  stops: ["b", "p", "d", "ɡ", "k", "t"],
+  fricatives: ["h", "f", "v", "s", "z", "ʃ", "ʒ", "ð", "θ"],
+  affricates: ["ʤ", "ʧ"],
+  nasals: ["m", "n", "ŋ"],
+  glides: ["w", "j"],
+  liquids: ["l", "ɹ"]
+};
